23590fe8516ec36c5ccabcedc80dc2bb463ef6a0
Kent C. Dodds <kent+github@doddsfamily.us>
2016-08-31 17:13:34 -0600

    fix(umd): do umd manually (#15)
    Rollup is not capable of exposing a default export AND a named export as
    a UMD module in a useful way for the global context. So this adds some
    custom UMD logic to the bottom of the main file. This should hopefully
    never need to be touched, but there are tests that validate the CJS and
    ES6 module exports.
    
    Things will be challenging when people want to use native ES6 modules
    with this module (because now we're not exposing this as a native ES6
    module). But we weren't really doing that in the first place (we were
    transpiling) so that's fine for now.
    
    Fixes #14

---
+++ dist-test/index.js
@@ -0,0 +1,65 @@
+/* eslint import/default:0, import/named:0, import/no-unresolved:0, import/extensions:0, no-console:0 */
+/*
+ * This file is here to validate that the built version of the library exposes the module in the way that we
+ * want it to. Specifically that the ES6 module import can get the matchSorter function via default import and the
+ * rankings via named import. Also that the CommonJS require returns the matchSorter function (rather than an object
+ * that has the matchSorter as a `default` property).
+ *
+ * This file is unable to validate the AMD or global exports.
+ */
+import assert from 'assert'
+
+import cjsImport, {rankings as cjsRankings} from '../dist/cjs'
+import umdImport, {rankings as umdRankings} from '../dist/umd/match-sorter'
+
+const cjsRequire = require('../dist/cjs')
+const umdRequire = require('../dist/umd/match-sorter')
+
+
+assert(
+  isMatchSorterFunction(cjsImport) && isRankingsObject(cjsRankings),
+ 'CJS build has a problem with ES6 modules'
+)
+
+assert(
+  isMatchSorterFunction(cjsRequire),
+  'CJS build has a problem with CJS'
+)
+
+assert(
+  isMatchSorterFunction(umdImport) && isRankingsObject(umdRankings),
+  'UMD build has a problem with ES6 modules'
+)
+
+assert(
+  isMatchSorterFunction(umdRequire),
+  'UMD build has a problem with CJS'
+)
+
+// TODO: how could we validate the AMD/global modules?
+
+console.log('Built modules look good 👍')
+
+function isMatchSorterFunction(thing) {
+  if (typeof thing !== 'function') {
+    console.error(`matchSorter thing should be a function. It's a ${typeof thing}`)
+    return false
+  }
+  if (thing.name !== 'matchSorter') {
+    console.error(`the function is not called "matchSorter". It's called ${thing.name}`)
+    return false
+  }
+  return isRankingsObject(thing.rankings)
+}
+
+function isRankingsObject(thing) {
+  if (typeof thing !== 'object') {
+    console.error(`rankings object thing should be an object. It's a ${typeof thing}`)
+    return false
+  }
+  if (!Object.keys(thing).includes('NO_MATCH')) {
+    console.error(`rankings object should include a NO_MATCH key. It only has ${Object.keys(thing)}`)
+    return false
+  }
+  return true
+}
--- package-scripts.js
+++ package-scripts.js
@@ -13,6 +13,14 @@ module.exports = {
         description: 'Run AVA in watch mode',
         script: 'ava -w --require babel-register',
       },
+      build: {
+        description: 'validates the built files',
+        script: 'babel-node dist-test/index.js',
+        watch: {
+          description: 'watches the dist directory for changes and reruns test.build when it changes',
+          script: watch(['dist-test/**/*.js', 'dist/**/*.js'], 'npm start -s test.build'),
+        },
+      },
     },
     build: {
       description: 'delete the dist directory and run all builds',
@@ -29,6 +37,20 @@ module.exports = {
           script: 'cross-env MINIFY=true rollup --config --sourcemap',
         },
       },
+      watch: {
+        description: 'watches the filesystem for changes and reruns the build when changes have been made',
+        script: watchSrc('npm start -s build'),
+      },
+    },
+    dev: {
+      build: {
+        description: 'helps while working on the module build and tests',
+        script: 'p-s -p build.watch,test.build.watch',
+      },
+      coverage: {
+        description: 'runs tests as files change in `src`',
+        script: watchSrc('p-s test'),
+      },
     },
     lint: {
       description: 'lint the entire project',
@@ -44,7 +66,7 @@ module.exports = {
     },
     validate: {
       description: 'This runs several scripts to make sure things look good before committing or on clean install',
-      script: 'p-s -p lint,build,test',
+      script: 'p-s -p lint,build,test && p-s test.build',
     },
     addContributor: {
       description: 'When new people contribute to the project, run this',
@@ -58,4 +80,12 @@ module.exports = {
   options: {
     silent: false,
   },
+}
+
+function watch(paths, command) {
+  return `nodemon --quiet ${paths.map(p => `--watch ${p}`).join(' ')} --exec "${command}"`
+}
+
+function watchSrc(command) {
+  return watch(['src/**/*.js'], command)
 }
