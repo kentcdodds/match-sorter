d1415195d8607919f02e17fcd399fc56cb725cc6
Kent C. Dodds <kent+github@doddsfamily.us>
2018-05-07 15:06:03 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -387,6 +387,7 @@ function getItemValues(item, key) {
     value = item[key]
   }
   // concat because `value` can be a string or an array
+  // eslint-disable-next-line
   return value != null ? [].concat(value) : null
 }
 
